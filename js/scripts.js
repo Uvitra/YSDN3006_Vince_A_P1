8a0f80f17b2675a7771bf23dcc91c1a28b34774f
Uvitra <Uvitra@users.noreply.github.com>
2018-10-07 21:33:30 -0400

    Initial Commit
    Commiting what I had done for a season concept. It wasn't the right approach, thus, a rewrite of sorts is in order.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/* global d3 */
+/* */
+
+// Global Javascript
+
+document.querySelector("p").setAttribute("style", "background-color: hsla(180, 75%, 75%, 0.75);");
+
+// D3 Related
+
+d3.select(".wins-graph").append("svg").attr("width", "100%").attr("height", "100%");
