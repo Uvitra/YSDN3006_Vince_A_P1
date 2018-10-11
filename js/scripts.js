5ce45e1fafff543a7a9e25bbe78d1a4e1347925a
Uvitra <Uvitra@users.noreply.github.com>
2018-10-11 15:18:30 -0400

    Redesigned the structure of the page
    Adding section byt section is next.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,4 @@ document.querySelector("p").setAttribute("style", "background-color: hsla(180, 7
 
 // D3 Related
 
-d3.select(".wins-graph").append("svg").attr("width", "100%").attr("height", "100%");
+d3.select(".wins-graph").append("svg").attr("width", "100%").attr("height", "100%").classed("wins-graph-svg", true);
