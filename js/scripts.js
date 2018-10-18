c71c62195c233dc54933ee8c9dfddd8690c0cf37
Uvitra <Uvitra@users.noreply.github.com>
2018-10-18 04:19:55 -0400

    added margin graph

--- js/scripts.js
+++ js/scripts.js
@@ -109,25 +109,35 @@ let gameMarginsData = d3.csv("../assets/data/201718RapsGames.csv", function (d) 
 	};
 });
 
-console.log(gameMarginsData);
+// console.log(gameMarginsData);
 
 gameMarginsData.then(function(d) {
-	let regularSeasonGames = d.slice(0, 82);
-	console.log(regularSeasonGames);
+	d = d.slice(0, 82);
+	// console.log(d);
 
-	let width = window.innerWidth;
-	let height = window.innerHeight;
-	let margin = {top: 20, right: 0, bottom: 30, left: 40};
-	let x, y, xAxis, yAxis;
+	let regularSeasonGamesDates = [];
 
-	x = d3.scaleBand()
-		.domain(d.map(d => d.name))
-		.range([margin.left, width - margin.right])
-		.padding(0.1);
+	for (var i = 0; i < d.length; i++) {
+		regularSeasonGamesDates[i] = d[i].date;
+	}
+
+	// console.log(d3.extent(regularSeasonGamesDates));
+
+	let width = window.innerWidth * 0.9;
+	let height = window.innerHeight * 0.5;
+	// let height = 200;
+	let margin = {top: 20, right: 20, bottom: 0, left: 20};
+	let x, y, xAxis, yAxis, colour;
+
+	colour = d3.scaleSequential(d3.interpolateRdYlGn);
+
+	x = d3.scaleTime()
+		.domain(d3.extent(regularSeasonGamesDates))
+		.range([0, width - margin.right]);
 
 	y = d3.scaleLinear()
-	    .domain([0, d3.max(d, d => d.value)]).nice()
-	    .range([height - margin.bottom, margin.top])
+		.domain([-20, d3.max(d, d => d.difference)]).nice()
+		.range([height - margin.bottom, margin.top]);
 
 	xAxis = g => g
 		.attr("transform", `translate(0,${height - margin.bottom})`)
@@ -135,12 +145,29 @@ gameMarginsData.then(function(d) {
 			.tickSizeOuter(0));
 
 	yAxis = g => g
-		.attr("transform", `translate(${margin.left},0)`)
+		.attr("transform", `translate(${margin.left-20},0)`)
 		.call(d3.axisLeft(y))
 		.call(g => g.select(".domain").remove());
 
 	let svg = d3.select(".game-margin-graph").append("svg").attr("width", "100%").attr("height", "100%").classed("game-margin-svg", true);
 	let chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
+
+	chartGroup.append("g")
+		.selectAll("rect").data(d).enter().append("rect")
+		.attr("fill", d => colour((d.difference + 20) / 50))
+		.attr("class", d => "bar bar--" + (d.difference < 0 ? "negative" : "positive"))
+		.attr("x", d => x(d.date))
+		.attr("y", d => d.difference < 0 ? y(0) : y(d.difference))
+		.attr("height", d => Math.abs(y(0) - y(d.difference)))
+		.attr("width", width/82+"px");
+
+	chartGroup.append("g")
+		.attr("class", "axis x")
+		.call(xAxis);
+
+	chartGroup.append("g")
+		.attr("class", "axis y")
+		.call(yAxis);
 
 });
 
