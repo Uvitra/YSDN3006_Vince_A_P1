0000126394fd0db60e4d9137a0e8fde64024f2fc
Uvitra <Uvitra@users.noreply.github.com>
2018-10-18 01:38:37 -0400

    Added "Let's go Raps" Section

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,12 @@ document.querySelector("p").setAttribute("style", "background-color: hsla(180, 7
 
 // D3 Related
 
+// d3.select(window).on("resize", callFunction);
+//
+// callFunction();
+// function callFunction() {
+
+// start of Wins Graph Code
 let winsGraphData = d3.csv("../assets/data/winsData.csv", function (d) {
 	// console.log(d);
 	return {
@@ -16,7 +22,7 @@ let winsGraphData = d3.csv("../assets/data/winsData.csv", function (d) {
 	};
 });
 
-console.log(winsGraphData);
+// console.log(winsGraphData);
 
 let dataDates = [];
 let winValues = [];
@@ -26,16 +32,16 @@ winsGraphData.then(function(d) {
 		dataDates[i] = d[i].date;
 		winValues[i] = d[i].winAmount;
 	}
-	console.log(dataDates);
+	// console.log(dataDates);
 	// expected output: list of dates
 
-  console.log(winValues);
+	// console.log(winValues);
 	// expected output: 24, 24, 23, 23, ...
 
-	console.log(d3.extent(dataDates));
+	// console.log(d3.extent(dataDates));
 
 	let height = window.innerHeight*.5;
-	let width = window.innerWidth*.8;
+	let width = (window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth)*.8;
 
 	let margin = {
 		left: 50,
@@ -49,7 +55,7 @@ winsGraphData.then(function(d) {
 		.range([0, width - margin.right]);
 
 	let y = d3.scaleLinear()
-		.domain([0, 60]).nice()
+		.domain([0, 70]).nice()
 		.range([height - margin.bottom, margin.top]);
 
 	let yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
@@ -57,7 +63,7 @@ winsGraphData.then(function(d) {
 		.attr("transform", `translate(0,${height - margin.bottom})`)
 		.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
 
-console.log(d.date);
+	// console.log(d.date);
 
 	let line = d3.line()
 		// .defined(d => !isNaN(d.value))
@@ -85,3 +91,22 @@ console.log(d.date);
 		.attr("d",line(d));
 
 });
+// end of wins-graph Code
+
+//Start of game margin graph
+
+let gameMarginsData = d3.csv("../assets/data/201718RapsGames.csv", function (d) {
+	console.log(d);
+	return {
+		city: d.city,
+		date: new Date(d.date.slice(0,4), d.date.slice(4,6)-1, d.date.slice(6,8)),
+		winAmount: +d.wins
+	};
+});
+
+// console.log(winsGraphData);
+
+//End of game margin graph
+
+
+// }
