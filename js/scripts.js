2de89b2c912285b5eb79ed3198873dd1e4ac0859
Uvitra <Uvitra@users.noreply.github.com>
2018-10-18 02:33:09 -0400

    Finished parsing the margin data files

--- js/scripts.js
+++ js/scripts.js
@@ -96,13 +96,54 @@ winsGraphData.then(function(d) {
 //Start of game margin graph
 
 let gameMarginsData = d3.csv("../assets/data/201718RapsGames.csv", function (d) {
-	console.log(d);
+	// console.log(d);
 	return {
-		city: d.city,
-		date: new Date(d.date.slice(0,4), d.date.slice(4,6)-1, d.date.slice(6,8)),
-		winAmount: +d.wins
+		attendence: +d.attendence,
+		date: new Date (d.date.slice(0,4), d.date.slice(5,7)-1, d.date.slice(8,10), d.date.slice(11,13), d.date.slice(14,16)),
+		difference: +d.difference,
+		homeTeam: d.home,
+		homePoints: +d.homePoints,
+		overtime: d.ot === "y" ? true : false,
+		visitorTeam: d.visitor,
+		visitorPoints: +d.visitorPoints
 	};
 });
+
+console.log(gameMarginsData);
+
+gameMarginsData.then(function(d) {
+	let regularSeasonGames = d.slice(0, 82);
+	console.log(regularSeasonGames);
+
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	let margin = {top: 20, right: 0, bottom: 30, left: 40};
+	let x, y, xAxis, yAxis;
+
+	x = d3.scaleBand()
+		.domain(d.map(d => d.name))
+		.range([margin.left, width - margin.right])
+		.padding(0.1);
+
+	y = d3.scaleLinear()
+	    .domain([0, d3.max(d, d => d.value)]).nice()
+	    .range([height - margin.bottom, margin.top])
+
+	xAxis = g => g
+		.attr("transform", `translate(0,${height - margin.bottom})`)
+		.call(d3.axisBottom(x)
+			.tickSizeOuter(0));
+
+	yAxis = g => g
+		.attr("transform", `translate(${margin.left},0)`)
+		.call(d3.axisLeft(y))
+		.call(g => g.select(".domain").remove());
+
+	let svg = d3.select(".game-margin-graph").append("svg").attr("width", "100%").attr("height", "100%").classed("game-margin-svg", true);
+	let chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
+
+});
+
 
 // console.log(winsGraphData);
 
