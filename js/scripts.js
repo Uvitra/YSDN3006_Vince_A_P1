23f6cb518b25baeda451ecbebca1c6d067aa6187
Uvitra <Uvitra@users.noreply.github.com>
2018-10-18 00:22:40 -0400

    Added initial wins over time graph
    added difference graph as well

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,6 @@ document.querySelector("p").setAttribute("style", "background-color: hsla(180, 7
 
 // D3 Related
 
-// var dataArray = [{x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10}];
-//
-// var svg = d3.select(".wins-graph").append("svg").attr("height","100%").attr("width","100%");
-//
-// var line = d3.line()
-// 	.x(function(d){ return d.x*6; })
-// 	.y(function(d){ return d.y*4; })
-// 	.curve(d3.curveStepAfter);
-//
-// svg.append("path")
-// 	.attr("fill","none")
-// 	.attr("stroke","blue")
-// 	.attr("d",line(dataArray));
-
 let winsGraphData = d3.csv("../assets/data/winsData.csv", function (d) {
 	// console.log(d);
 	return {
@@ -48,8 +34,8 @@ winsGraphData.then(function(d) {
 
 	console.log(d3.extent(dataDates));
 
-	let height = 200;
-	let width = 350;
+	let height = window.innerHeight*.5;
+	let width = window.innerWidth*.8;
 
 	let margin = {
 		left: 50,
@@ -63,7 +49,7 @@ winsGraphData.then(function(d) {
 		.range([0, width - margin.right]);
 
 	let y = d3.scaleLinear()
-		.domain([0, 82]).nice()
+		.domain([0, 60]).nice()
 		.range([height - margin.bottom, margin.top]);
 
 	let yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
@@ -71,10 +57,18 @@ winsGraphData.then(function(d) {
 		.attr("transform", `translate(0,${height - margin.bottom})`)
 		.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
 
+console.log(d.date);
+
+	let line = d3.line()
+		// .defined(d => !isNaN(d.value))
+		.x(function(d){ return x(d.date); })
+		.y(function(d){ return y(d.winAmount); })
+		.curve(d3.curveStepAfter);
+
 	let svg = d3.select(".wins-graph").append("svg").attr("width", "100%").attr("height", "100%").classed("wins-graph-svg", true);
 	let chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
 
-	// chartGroup.append("path").attr("d",area(dataArray));
+	// chartGroup.append("path").attr("d", area(dataArray));
 
 	chartGroup.append("g")
 		.attr("class","axis y")
@@ -84,5 +78,10 @@ winsGraphData.then(function(d) {
 		.attr("class","axis x")
 		.attr("transform","translate(0,"+height+")")
 		.call(xAxis);
+
+	chartGroup.append("path")
+		.attr("fill", "none")
+		.attr("stroke", "blue")
+		.attr("d",line(d));
 
 });
